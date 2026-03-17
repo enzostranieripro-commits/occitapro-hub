19fda75c8eb2c8a9f4820543715f0ff0bf0b694f
enzostranieripro-commits <enzostranieri.pro@gmail.com>
2026-03-17 21:52:23 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,7 @@ import NotFound from "./pages/NotFound";
 const Auth = lazy(() => import('./pages/Auth'));
 const Onboarding = lazy(() => import('./pages/Onboarding'));
 const Dashboard = lazy(() => import('./pages/Dashboard'));
+const SuperAdmin = lazy(() => import('./pages/SuperAdmin'));
 
 const queryClient = new QueryClient();
 
